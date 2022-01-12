01829d80337aa3697b9482d9d9cabadce665ee00
Jasjit Rudra <50959696+JasjitSinghRudra@users.noreply.github.com>
2022-01-12 18:52:05 +0530

    Updated Index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php 
- 
-  
-    include 'core/init.php' ;
-    
+   
+    include 'core/init.php' ;    
     if (isset($_SESSION['user_id'])) {
       header('location: home.php');
     }
-   
 ?>
 
 <html>
@@ -15,76 +12,126 @@
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
         <!-- <link rel="stylesheet" href="assets/css/style-complete.css"/> -->
-        <link rel="stylesheet" href="assets/css/index_style.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
-		<link rel="shortcut icon" type="image/png" href="https://cdn-icons.flaticon.com/png/512/461/premium/461146.png?token=exp=1639386067~hmac=226052811f9712abd43effaba8ddbad6"> 
+		<link rel="shortcut icon" type="image/png" href="https://www.pinclipart.com/picdir/middle/24-247211_rejected-stamp-clipart-guitar-cartoon-guitar-transparent-background.png"> 
+    <style type="text/css">
+      body {
+    font-family: "Lato", sans-serif;
+}
+.main-head{
+    height: 150px;
+    background: #FFF;   
+}
+.sidenav {
+    height: 100%;
+    background-color: #000;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+.main {
+    padding: 0px 10px;
+}
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+}
+@media screen and (max-width: 450px) {
+    .login-form{
+        margin-top: 10%;
+    }
+    .register-form{
+        margin-top: 10%;
+    }
+}
+@media screen and (min-width: 768px){
+    .main{
+        margin-left: 42%; 
+    }
+    .sidenav{
+        width: 40%;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+    }
+
+    .login-form{
+        margin-top: 16%;
+    }
+
+    .register-form{
+        margin-top: 20%;
+    }
+}
+.login-main-text{
+    margin-top: 16%;
+    padding: 60px;
+    color: #fff;
+}
+.login-main-text h2{
+    font-weight: 300;
+}
+.btn-primary{
+    background-color: #3e96f4 !important;
+    color: #fff;
+}
+    </style>
 	</head>
-    <nav class="navbar navbar-light" style="background-color: #da5775;">
-        <a class="navbar-brand" href="#">
-          Ceol
-      </a>
-    </nav>
+   
 <body>
-<?php include 'includes/login.php';  ?>
 
-<section class="sign-card">
-  
-  <div class="container">  
-  If you havent created an account then create one now!
-  <hr>
-<button type="button" id="auto" onclick="" class="signup-btn pri-btn" data-toggle="modal" data-target="#exampleModalCenter">
-                    Sign Up</button>
-                   
+<nav class="navbar navbar-dark" style="background-color: #393f45;">
+  <p>&nbsp;</p>
+</nav>
+<div class="sidenav" style="background:url('http://www.wallpaperup.com/uploads/wallpapers/2012/10/21/20181/cad2441dd3252cf53f12154412286ba0.jpg');">
+         <div class="login-main-text">
+            <h2>Ceol</h2>
+            <p>Welcome to the world of Artists</p>
+         </div>
+      </div>
+      <div class="main">
+         <div class="col-md-8 col-sm-8">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>User Name</label>
+                     <input type="text" class="form-control" placeholder="User Name">
+                  </div>
+                  <div class="form-group">
+                     <label>Password</label>
+                     <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-info">Login</button>
+                  <br>
+                  <br>
+                  <br>
+                  <P>Don't have an account?</P><p> Sign-up here </p>
+                  <button type="submit" class="btn btn-primary">Register</button>
+               </form>
+            </div>
+         </div>
+      </div>
+      <nav class="navbar fixed-bottom navbar-expand-lg navbar-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact and Support</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+        &#169; Jasjit and Jyotik
+      </li>
+    </ul>
   </div>
-  </section>                    
-                    <!-- Modal -->
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-  <div class="modal-content">
-  <div class="modal-header">
-  <h5 style="font-weight: 700;" class="modal-title" id="exampleModalLongTitle">Sign Up</h5>
-  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-  <span aria-hidden="true">&times;</span>
-</button>
-</div>
-<div class="modal-body">
-            
- <?php  include 'includes/signup-form.php' ?>
-</div>
-</div>
-</div>
-</div>
-  
-
-	<div class="container-2">
-   Welcome to the world of Artists
-   <div class="feat">
-   <ol>
-    <li>You can have great conversations with fellow artists!</li>
-    <li>No registration Fees, just fun</li>
-    <li>Connect and make music ! <li>
-  </div> 
-  </ol>
-  </div>	
-		
-
-            <footer>
-                <ol>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Status</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Advertise</a></li>
-                    <li>© 2021 Jasjit And Jyotik</li>
-                </ol>
-            </footer>
-        </main>
-        
-        <script src="assets/js/jquery-3.4.1.slim.min.js"></script>
-        <script src="assets/js/popper.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/mine.js"></script>
+  </nav>
 </body>
 </html>
